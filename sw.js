f0cca070a161d943c91739efdbfae84ceb28896a
Dinocrys <87445249+Dinocrys@users.noreply.github.com>
2026-09-13 10:34:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='karten-pokedex-v3-21';
+const CACHE='karten-pokedex-v3-22';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
